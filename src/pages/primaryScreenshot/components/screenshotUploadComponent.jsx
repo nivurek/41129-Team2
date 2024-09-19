5e73dda9cdae5f315c2beb82bf9bda2bbc471994
lorkingcameron <lorkingcameron@gmail.com>
2024-09-20 03:31:37 +1000

    Added Remove File Functionality

--- src/pages/primaryScreenshot/components/screenshotUploadComponent.jsx
+++ src/pages/primaryScreenshot/components/screenshotUploadComponent.jsx
@@ -28,13 +28,15 @@ const ScreenshotUpload = ({updateResults}) => {
     }, [data])
 
     const onUpload = (event) => {console.log(event);};
+
+    const onRemove = (event) => {setImageUrl('');};
     
     const uploadHandler = (event) => {
         setImageUrl(event.files[0].objectURL);
     };
 
     return (
-        <FileUpload className="w-30rem" accept="image/*" maxFileSize={1000000} customUpload="true" uploadHandler={uploadHandler} onUpload={onUpload} auto chooseLabel="Browse" />
+        <FileUpload className="w-30rem" accept="image/*" maxFileSize={1000000} customUpload="true" uploadHandler={uploadHandler} onUpload={onUpload} onRemove={onRemove} auto chooseLabel="Browse" />
     );
 };
 
