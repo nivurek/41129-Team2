b3ef7dfeeea22800c17df34ab5d72b48ad17b016
lorkingcameron <lorkingcameron@gmail.com>
2024-10-16 12:27:40 +1100

    Upgraded color extraction to Vibrant

--- src/pages/PrimaryScreenshot/components/screenshotUploadComponent.jsx
+++ src/pages/PrimaryScreenshot/components/screenshotUploadComponent.jsx
@@ -1,18 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { FileUpload } from "primereact/fileupload";
-import { usePalette } from 'react-palette'
+import * as Vibrant from 'node-vibrant'
 
 const ScreenshotUploadComponent = ({updateImageColorPalette}) => {
     const [imageUrl, setImageUrl] = useState('');
-
-    const { data } = usePalette(imageUrl);
+    const [data, setData] = useState({});
+    const imageSrc = React.createRef();
 
     useEffect(() => {
-        console.log('data', data);
-        delete data.lightMuted;
+        if (Object.keys(data).length === 0) return
+        console.log(data);
+        delete data.LightMuted;
 
-        updateImageColorPalette({...data});
+        var processedData = {
+            vibrant: data.Vibrant.hex,
+            lightVibrant: data.LightVibrant.hex,
+            darkVibrant: data.DarkVibrant.hex,
+            muted: data.Muted.hex,
+            darkMuted: data.DarkMuted.hex
+        }
+
+        console.log(processedData);
+
+        updateImageColorPalette({...processedData});
     }, [data])
+
+    useEffect(() => {
+        if (!imageUrl) {updateImageColorPalette({}); return;}
+        Vibrant.from(imageUrl).quality(1).getPalette((err, palette) => {setData(palette); console.log(data)})
+    }, [imageUrl])
 
     const onUpload = (event) => {console.log(event);};
 
@@ -23,7 +39,10 @@ const ScreenshotUploadComponent = ({updateImageColorPalette}) => {
     };
 
     return (
-        <FileUpload className="w-30rem" accept="image/*" maxFileSize={1000000} customUpload="true" uploadHandler={uploadHandler} onUpload={onUpload} onRemove={onRemove} auto chooseLabel="Browse" />
+        <div>
+            <FileUpload className="w-30rem" accept="image/*" maxFileSize={1000000} customUpload="true" uploadHandler={uploadHandler} onUpload={onUpload} onRemove={onRemove} auto chooseLabel="Browse" />
+            <img ref={imageSrc} src={imageUrl} alt="No Img" style={{width:'100px', height: '100px'}} />
+        </div>
     );
 };
 
