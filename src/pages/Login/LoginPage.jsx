ff2664d7b562f10d629fc5f8d2e01ef546eaff1b
lorkingcameron <lorkingcameron@gmail.com>
2024-10-16 12:27:13 +1100

    Removed unused deps

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,15 +1,9 @@
 import React, { useState } from 'react';
 import {
   Button,
-  ButtonGroup,
-  Form,
   Grid,
-  Header,
-  Icon,
-  Message,
   Menu,
   MenuItem,
-  Segment,
 } from 'semantic-ui-react';
 import { useNavigate } from 'react-router-dom';
 
